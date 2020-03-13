8e37c9319431a078bb40d64447cac04314b29401
BradHammond <brad@exp.org.au>
2020-03-13 23:46:48 +1100

    Did dots events tute

---
+++ Assets/_Sandbox/Tutes/Code Monkey tutes/How to handle events in DOTs/PipeMoveSystemBase.cs
@@ -0,0 +1,35 @@
+﻿using Unity.Entities;
+using Unity.Transforms;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+using System;
+using UnityEngine;
+
+/*
+public class PipeMoveSystemBase : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float deltaTime = Time.DeltaTime;
+        float3 moveDir = new float3(-1, 0, 0);
+        float moveSpeed = 4;
+       
+        JobHandle jobHandle = Entities.ForEach((ref Translation translation, ref Pipe pipe) =>
+        {
+            float xBefore = translation.Value.x;
+            translation.Value += moveDir * moveSpeed * deltaTime;
+            float xAfter = translation.Value.x;
+
+            if(xBefore > 0 && xAfter <= 0)
+            {
+                // Passed the player
+                Debug.Log("Here");
+            }
+
+        }).Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
+*/
---
+++ Assets/_Sandbox/Tutes/Code Monkey tutes/How to handle events in DOTs/TestingDOTsEventsV1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class TestingDOTsEventsV1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        World.DefaultGameObjectInjectionWorld.GetOrCreateSystem<PipeMoveSystemV2>().OnPassedXEvent += TestingDOTsEvents_OnPipePassed;
+    }
+
+    private void TestingDOTsEvents_OnPipePassed(object sender, System.EventArgs e)
+    {
+        Debug.Log("Passed x0.");
+    }
+}
