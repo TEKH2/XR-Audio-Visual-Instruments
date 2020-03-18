069b62da36e9f1ed1a166bee147f4845f563a3ce
BradHammond <brad@exp.org.au>
2020-03-18 16:59:19 +1100

    imported painter without newtonsoft json. no errors

---
+++ Assets/_Framework/Packages/Painter 3D/Scripts/UI/PainterDebugUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using EXP;
+using EXP.XR;
+
+namespace EXP.Painter
+{
+    public class PainterDebugUI : MonoBehaviour
+    {
+        public TextMeshProUGUI _DebugText;
+        public TextMeshProUGUI _DebugTextController;
+        public Brush _Brush;
+
+        void Update()
+        {
+            if (_Brush.Painting)
+                _DebugText.text = "Painting";
+            else
+                _DebugText.text = "Not Painting";
+
+            _DebugTextController.text = XRControllers.Instance._RightControllerFeatures._XRFloatDict[XRFloats.Trigger].Value.ToString();
+        }
+    }
+}
