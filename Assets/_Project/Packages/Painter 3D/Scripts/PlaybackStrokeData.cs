cc93bcea7cf530edd9daec4d9bdd827fb0e5eaa8
BradHammond <brad@exp.org.au>
2020-03-26 14:04:56 +1100

    Added particle visuals and events too the playback

--- Assets/_Project/Packages/Painter 3D/Scripts/PlaybackStrokeData.cs
+++ Assets/_Project/Packages/Painter 3D/Scripts/PlaybackStrokeData.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace EXP.Painter
 {
@@ -11,6 +12,9 @@ namespace EXP.Painter
             Paused,
             Playing,
         }
+
+        
+
 
         public float _Timer;
         public float _PlaybackSpeed = 1;
@@ -31,11 +35,24 @@ namespace EXP.Painter
 
         public Transform _PlaybackHeadTransform;
 
+        public ParticleSystem _PS;
+        ParticleSystem.MainModule _PSMain;
+
+        public Color _LowAngle;
+        public Color _HighAngle;
+        public float _MaxSpeed = 2f;
+
+
+        public FloatEvent _Out_Speed;
+        public Vector3Event _Out_Direction;
+        public FloatEvent _Out_AngleChange;
+
 
         // Start is called before the first frame update
         void Start()
         {
             FindObjectOfType<PainterCanvas>().OnStrokeAdded += OnStrokeAdded;
+            _PSMain = _PS.main;
         }
 
         void OnStrokeAdded(BrushStroke brushStroke)
@@ -46,14 +63,28 @@ namespace EXP.Painter
         // Update is called once per frame
         void Update()
         {
+            if(Input.GetKeyDown(KeyCode.P))
+            {
+                _State = State.Playing;
+            }
+
             if(_State == State.Playing && _ActiveStroke != null)
             {
                 _Timer += Time.deltaTime * _PlaybackSpeed;
+             
 
                 StrokeNode strokeNode = _ActiveStroke.GetNodeAtTime(_Timer);
 
                 // update position
                 _PlaybackHeadTransform.position = strokeNode.OriginalPos;
+
+                _PSMain.startColor = Color.Lerp(_LowAngle, _HighAngle, strokeNode._Speed / _MaxSpeed);
+                print(strokeNode._NormAngleChange);
+
+                //Output
+                _Out_Speed?.Invoke(strokeNode._Speed);
+                _Out_Direction?.Invoke(strokeNode._Direction);
+                _Out_AngleChange?.Invoke(strokeNode._NormAngleChange);
             }
         }
     }
