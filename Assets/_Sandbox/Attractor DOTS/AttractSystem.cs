19767485ee4ebcec2cdb206677751275162b1dff
BradHammond <brad@exp.org.au>
2020-07-24 11:56:32 +1000

    checking dots physics

--- Assets/_Sandbox/Attractor DOTS/AttractSystem.cs
+++ Assets/_Sandbox/Attractor DOTS/AttractSystem.cs
@@ -14,55 +14,84 @@ using Unity.Burst;
 
 
 [UpdateBefore(typeof(BuildPhysicsWorld))]
-public class AttractSystem : JobComponentSystem
+public class AttractSystem : SystemBase
 {
-    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    protected override void OnUpdate()
     {
-        NativeArray<Entity> attractorEntities = GetEntityQuery(typeof(AttractComponent), typeof(Translation)).ToEntityArray(Allocator.TempJob);
+        EntityQuery entityQuery = GetEntityQuery(typeof(AttractComponent), typeof(Translation));
+        NativeArray<AttractComponent> attractorComps = entityQuery.ToComponentDataArray<AttractComponent>(Allocator.TempJob);
+        NativeArray<Translation> attractorTransComps = entityQuery.ToComponentDataArray<Translation>(Allocator.TempJob);
 
-        JobHandle attractJobHandle = new AttractJob
-        {
-            attractorEntities = attractorEntities,
-            attractComponents = GetComponentDataFromEntity<AttractComponent>(true),
-            attractTranslationComponents = GetComponentDataFromEntity<Translation>(true),
-        }.Schedule(this, inputDeps);
-
-        attractJobHandle.Complete();
-
-        attractorEntities.Dispose();
-
-        return attractJobHandle;
-    }
-
-    [BurstCompile]
-    struct AttractJob : IJobForEach<PhysicsVelocity, Translation>
-    {
-        [ReadOnly] public NativeArray<Entity> attractorEntities;
-        [ReadOnly] public ComponentDataFromEntity<AttractComponent> attractComponents;
-        [ReadOnly] public ComponentDataFromEntity<Translation> attractTranslationComponents;
-
-        public void Execute(ref PhysicsVelocity physVelComponent, [ReadOnly] ref Translation attractableTranslation)
-        {
-            float3 attraction = new float3(0, 0, 0);
-            for (int i = 0; i < attractorEntities.Length; i++)
-            {
-                Entity entity = attractorEntities[i];
-                AttractComponent attractorComp = attractComponents[entity];
-                Translation attractorTranslation = attractTranslationComponents[entity];
-
-                //Debug.Log(entity + "    " + attractorTranslation.Value);
-
-                float3 vectorToAttractor = attractorTranslation.Value - attractableTranslation.Value;
-                float distSqrd = math.lengthsq(vectorToAttractor);
-
-                if (distSqrd < attractorComp._MaxDistanceSqrd)
+        // Update cube volume effect
+        Entities.ForEach
+        (
+            (ref PhysicsVelocity physVelComponent, ref Translation translation) =>
+            {                
+                float3 attraction = new float3(0, 0, 0);
+                for (int i = 0; i < attractorComps.Length; i++)
                 {
-                    // Alter linear velocity
-                    attraction += attractorComp._Strength * (vectorToAttractor / math.sqrt(distSqrd));
-                }
-            }
+                    float3 vectorToAttractor = attractorTransComps[i].Value - translation.Value;
+                    float distSqrd = math.lengthsq(vectorToAttractor);
 
-            physVelComponent.Linear += attraction;
-        }
+                    if (distSqrd < attractorComps[i]._MaxDistanceSqrd)
+                    {
+                        // Alter linear velocity
+                        attraction += attractorComps[i]._Strength * (vectorToAttractor / math.sqrt(distSqrd));
+                    }                    
+                }
+
+                physVelComponent.Linear += attraction;
+            }
+        ).WithDeallocateOnJobCompletion(attractorComps).WithDeallocateOnJobCompletion(attractorTransComps).ScheduleParallel();
     }
-};
+
+        //protected override JobHandle OnUpdate(JobHandle inputDeps)
+        //{
+        //    NativeArray<Entity> attractorEntities = GetEntityQuery(typeof(AttractComponent), typeof(Translation)).ToEntityArray(Allocator.TempJob);
+
+        //    JobHandle attractJobHandle = new AttractJob
+        //    {
+        //        attractorEntities = attractorEntities,
+        //        attractComponents = GetComponentDataFromEntity<AttractComponent>(true),
+        //        attractTranslationComponents = GetComponentDataFromEntity<Translation>(true),
+        //    }.Schedule(this, inputDeps);
+
+        //    attractJobHandle.Complete();
+
+        //    attractorEntities.Dispose();
+
+        //    return attractJobHandle;
+        //}
+
+        //[BurstCompile]
+        //struct AttractJob : IJobForEach<PhysicsVelocity, Translation>
+        //{
+        //    [ReadOnly] public NativeArray<Entity> attractorEntities;
+        //    [ReadOnly] public ComponentDataFromEntity<AttractComponent> attractComponents;
+        //    [ReadOnly] public ComponentDataFromEntity<Translation> attractTranslationComponents;
+
+        //    public void Execute(ref PhysicsVelocity physVelComponent, [ReadOnly] ref Translation attractableTranslation)
+        //    {
+        //        float3 attraction = new float3(0, 0, 0);
+        //        for (int i = 0; i < attractorEntities.Length; i++)
+        //        {
+        //            Entity entity = attractorEntities[i];
+        //            AttractComponent attractorComp = attractComponents[entity];
+        //            Translation attractorTranslation = attractTranslationComponents[entity];
+
+        //            //Debug.Log(entity + "    " + attractorTranslation.Value);
+
+        //            float3 vectorToAttractor = attractorTranslation.Value - attractableTranslation.Value;
+        //            float distSqrd = math.lengthsq(vectorToAttractor);
+
+        //            if (distSqrd < attractorComp._MaxDistanceSqrd)
+        //            {
+        //                // Alter linear velocity
+        //                attraction += attractorComp._Strength * (vectorToAttractor / math.sqrt(distSqrd));
+        //            }
+        //        }
+
+        //        physVelComponent.Linear += attraction;
+        //    }
+        //}
+    };
