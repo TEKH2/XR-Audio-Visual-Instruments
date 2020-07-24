e2876bc6bb732bc74f31a250d13a3ee9c17c1c81
BradHammond <brad@exp.org.au>
2020-07-24 16:28:11 +1000

    adding point cloud instrument

---
+++ Assets/_Sandbox/Pointcloud instrument/GrainDataObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrainDataObject : MonoBehaviour
+{
+    public GrainEmissionProps _GrainEmissionProps;
+}
---
+++ Assets/_Sandbox/Pointcloud instrument/PointCloudInstrument.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointCloudInstrument : MonoBehaviour
+{
+    public Granulator_MultiData _Granulator;
+
+    GrainDataObject[] _AllGrainDataObjects;
+
+    public float _Radius = .3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _AllGrainDataObjects = FindObjectsOfType<GrainDataObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < _AllGrainDataObjects.Length; i++)
+        {
+
+        }
+    }
+}
