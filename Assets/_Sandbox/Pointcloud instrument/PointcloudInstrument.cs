be3fa0c5d4990d13332bf1bafadfc9a21d577111
BradHammond <brad@exp.org.au>
2020-08-21 16:14:16 +1000

    Added point cloud VFX base

---
+++ Assets/_Sandbox/Pointcloud instrument/PointcloudInstrument.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+[ExecuteInEditMode]
+public class PointcloudInstrument : MonoBehaviour
+{
+    public Transform _InputTform;
+    public VisualEffect _VFX;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _VFX.SetVector3("InputPos-Hand", _InputTform.position);
+        _VFX.SetFloat("InputRadius-Hand", _InputTform.localScale.x*.5f);
+    }
+}
