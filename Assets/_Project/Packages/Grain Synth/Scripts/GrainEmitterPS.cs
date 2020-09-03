e68b9024451325d0cf702b3a003ce26118f4f0fa
BradHammond <brad@exp.org.au>
2020-09-03 16:55:18 +1000

    added PS trying to figure out why the mouse pointer doesnt line up

---
+++ Assets/_Project/Packages/Grain Synth/Scripts/GrainEmitterPS.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrainEmitterPS : MonoBehaviour
+{
+    ParticleSystem _PS;
+    ParticleSystem.EmissionModule _Emission;
+    ParticleSystem.MainModule _Main;
+
+    public GrainEmitterAuthoring _GrainEmitter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _PS = GetComponent<ParticleSystem>();
+        _Emission = _PS.emission;
+        _Main = _PS.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _Main.startLifetime = _GrainEmitter._EmissionProps.Duration * .001f;
+        _Emission.rateOverTime = 1000f / _GrainEmitter._EmissionProps.Cadence;
+    }
+}
