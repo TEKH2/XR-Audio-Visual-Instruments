446f5ab942d40ce573b8591ebcec4f9f9f1d9d15
BradHammond <brad@exp.org.au>
2020-09-04 17:23:31 +1000

    vizualizer now uses actual objects and is far more exact

--- Assets/_Project/Packages/Grain Synth/Scripts/GrainSynthVisualizer.cs
+++ Assets/_Project/Packages/Grain Synth/Scripts/GrainSynthVisualizer.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
+
 public class GrainSynthVisualizer : MonoBehaviour
 {
     ParticleSystem _PS;
@@ -16,7 +16,7 @@ public class GrainSynthVisualizer : MonoBehaviour
 
     float _SampleRate;
     public int _HeightCount = 4;
-    int _Increment = 0;
+    int _IncrementY = 0;
 
     public Transform _XAxisPivot;
     public Transform _XAxisPivot_Frametime;
@@ -24,8 +24,15 @@ public class GrainSynthVisualizer : MonoBehaviour
 
     public GrainSpeakerAuthoring _GrainSpeaker;
 
-    
-    
+
+    GrainSynthVisualizerBlock[] _Blocks;
+    int _PoolAmount = 200;
+    int _BlockCounter = 0;
+    public GrainSynthVisualizerBlock _BlockPrefab;
+
+    public bool _EmitParticles = true;
+    public bool _EmitBlocks = true;
+
 
     // Start is called before the first frame update
     void Start()
@@ -38,6 +45,14 @@ public class GrainSynthVisualizer : MonoBehaviour
 
         _SampleRate = AudioSettings.outputSampleRate;
         _GrainSpeaker.OnGrainEmitted += EmitGrain;
+
+        _Blocks = new GrainSynthVisualizerBlock[_PoolAmount];
+        for (int i = 0; i < _Blocks.Length; i++)
+        {
+            GrainSynthVisualizerBlock newBlock = Instantiate(_BlockPrefab, transform);
+            newBlock.gameObject.SetActive(false);
+            _Blocks[i] = newBlock;
+        }
     }
 
     // Update is called once per frame
@@ -51,49 +66,82 @@ public class GrainSynthVisualizer : MonoBehaviour
 
         if (Application.isPlaying && _XAxisPivot_Frametime != null)
             _XAxisPivot_Frametime.SetScaleX(-GrainSynth.Instance._EmissionLatencyMS * .001f * (_Distance / _Lifetime));
+
+        for (int i = 0; i < _Blocks.Length; i++)
+        {
+            if(_Blocks[i].gameObject.activeSelf)
+            {
+                int sampleDiff = _Blocks[i]._StartIndex - GrainSynth.Instance._CurrentDSPSample;
+                Vector3 pos = transform.position + transform.right * (sampleDiff / _SampleRate) * (_Distance / _Lifetime);
+                pos.y = _Blocks[i].transform.position.y;
+
+                _Blocks[i].transform.position = pos;
+            }
+        }
+    }
+
+    Vector3 PosFromStartSampleIndex(int startSampleIndex)
+    {
+        int sampleDiff = startSampleIndex - GrainSynth.Instance._CurrentDSPSample;
+        Vector3 pos = transform.position + transform.right * (sampleDiff / _SampleRate) * (_Distance / _Lifetime);
+
+        pos.y += (_IncrementY % _HeightCount) * _Scale;
+        pos.y += _Scale * .5f;
+        _IncrementY++;
+
+        return pos;
     }
 
     int prevEmitSample;
     int sampleGap;
     public void EmitGrain(GrainPlaybackData grainData, int currentDSPSample)
     {
-        // DEBUG
-        sampleGap = (int)(_SampleRate * .001f * 20);
-        int sampleTIming = grainData._DSPStartIndex - prevEmitSample;
-        if (sampleTIming != sampleGap)
-            print(sampleTIming + "   " + sampleGap);
-
-        prevEmitSample = grainData._DSPStartIndex;
-
-        ParticleSystem.EmitParams emit = new ParticleSystem.EmitParams();
-
-        // Position based on start index
-        int sampleDiff = grainData._DSPStartIndex - currentDSPSample;
-        Vector3 worldPos = transform.position + transform.right * (sampleDiff / _SampleRate) * (_Distance / _Lifetime);
-        worldPos.y += (_Increment % _HeightCount) * _Scale;
-        worldPos.y += _Scale * .5f;
-        emit.position = worldPos;
-
         // Scale based on duration
         float durationInSeconds = grainData._PlaybackSampleCount / _SampleRate;
         durationInSeconds *= _Distance / _Lifetime;
         Vector3 size = new Vector3(durationInSeconds, _Scale, .001f);
-        emit.startSize3D = size;
 
-        emit.startColor = Color.white;
+        if (_EmitBlocks)
+        {
+            GrainSynthVisualizerBlock block = _Blocks[_BlockCounter];
+            block.transform.position = PosFromStartSampleIndex(grainData._DSPStartIndex);
+            block.transform.localScale = size;
+            block._StartIndex = grainData._DSPStartIndex;
+            block.gameObject.SetActive(true);
+            _BlockCounter++;
+            _BlockCounter %= _Blocks.Length;
+        }
 
-        if (grainData._DSPStartIndex <= currentDSPSample)
-            emit.startColor = Color.yellow;
 
-        // Velocity based on lifetime/dist
-        emit.velocity = -transform.right * (_Distance/_Lifetime);
+        // DEBUG
+        //sampleGap = (int)(_SampleRate * .001f * 20);
+        //int sampleTIming = grainData._DSPStartIndex - prevEmitSample;
+        //if (sampleTIming != sampleGap)
+        //    print(sampleTIming + "   " + sampleGap);
 
-        _PS.Emit(emit, 1);
+        prevEmitSample = grainData._DSPStartIndex;
 
-        _Increment++;
+        if (_EmitParticles)
+        {
+            ParticleSystem.EmitParams emit = new ParticleSystem.EmitParams();
+
+            // Position based on start index       
+            emit.position = PosFromStartSampleIndex(grainData._DSPStartIndex);
+
+
+            emit.startSize3D = size;
+
+            emit.startColor = Color.white;
+
+            if (grainData._DSPStartIndex <= currentDSPSample)
+                emit.startColor = Color.yellow;
+
+            // Velocity based on lifetime/dist
+            emit.velocity = -transform.right * (_Distance / _Lifetime);
+
+            _PS.Emit(emit, 1);
+        }
     }
-
-
 
     private void OnDrawGizmos()
     {
---
+++ Assets/_Project/Packages/Grain Synth/Scripts/GrainSynthVisualizerBlock.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrainSynthVisualizerBlock : MonoBehaviour
+{
+    public int _StartIndex;
+    public float _StartX;
+}
