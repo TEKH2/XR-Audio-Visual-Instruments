343619c8fcccb485ea73a53fb094d12af4839953
BradHammond <brad@exp.org.au>
2020-09-16 12:28:37 +1000

    waveform visualizer and playhead working

--- Assets/_Project/Packages/Grain Synth/Scripts/GrainSynthVisualizer.cs
+++ Assets/_Project/Packages/Grain Synth/Scripts/GrainSynthVisualizer.cs
@@ -10,16 +10,22 @@ public class GrainSynthVisualizer : MonoBehaviour
 {
     #region ------------------------------------- VARIABLES 
     float _SampleRate;
-   
+    public GrainEmitterAuthoring _Emitter;
+    Vector3 _LookAtPos;
 
     // ----------------------------------- WAVEFORM/PLAYHEAD
     [Header("Waveform - Playhead")]
     public float _ArcTotalAngle = 90;    
     public float _ArcRadius = 4;
     public int _WaveformBlockCount = 200;
+    public float _WaveformHeight = 1;
     public float _WaveformBlockHeight = .5f;
     public GameObject _WaveformBlockPrefab;
     GameObject[] _WaveformBlocks;
+    public LineRenderer _WaveformBorderLine;
+    public int _BorderVerts = 30;
+
+    public Transform _WaveformPlayhead;
 
     // ----------------------------------- TIMELINE
     [Header("Timeline")]
@@ -60,31 +66,61 @@ public class GrainSynthVisualizer : MonoBehaviour
         _WaveformBlocks = new GameObject[_WaveformBlockCount];
         float waveformBlockWidth = Vector3.Distance(GetPositionOnArc(0), GetPositionOnArc(1f/ (_WaveformBlockCount-1) ) );
         float[] averagedSamples = new float[_WaveformBlockCount];
+
+        _LookAtPos = Vector3.up * _WaveformHeight;
+
+        int index = 0;
         for (int i = 0; i < _WaveformBlockCount; i++)
         {
-            float currentSample = 0;
+            float sumedSquaredSamples = 0;
             for (int s = 0; s < samplesPerBlock; s++)
             {
-                int index = i * s + s;
-                currentSample += clipData[index];
+                sumedSquaredSamples += clipData[index] * clipData[index]; // sum squared samples
+                index++;
             }
 
-            currentSample /= samplesPerBlock;
-            maxSampleValue = Mathf.Max(currentSample, maxSampleValue);
-            minSampleValue = Mathf.Min(currentSample, minSampleValue);
-            averagedSamples[i] = currentSample;
+            sumedSquaredSamples /= samplesPerBlock;
+            float rmsValue = Mathf.Sqrt(sumedSquaredSamples / samplesPerBlock); // rms = square root of average
+
+            maxSampleValue = Mathf.Max(rmsValue, maxSampleValue);
+            minSampleValue = Mathf.Min(rmsValue, minSampleValue);
+            averagedSamples[i] = rmsValue;
 
             float norm = i / (_WaveformBlockCount - 1f);
             _WaveformBlocks[i] = Instantiate(_WaveformBlockPrefab, transform);
             _WaveformBlocks[i].transform.position = GetPositionOnArc(norm);
-            _WaveformBlocks[i].transform.LookAt(Vector3.zero);
+            _WaveformBlocks[i].transform.LookAt(_LookAtPos);
+            _WaveformBlocks[i].transform.rotation *= Quaternion.Euler(0, 180, 0);
         }
 
         for (int i = 0; i < _WaveformBlockCount; i++)
         {
             float height = Mathf.InverseLerp(minSampleValue, maxSampleValue, averagedSamples[i]);
-            _WaveformBlocks[i].transform.localScale = new Vector3(waveformBlockWidth, height * _WaveformBlockHeight, .01f);
+            _WaveformBlocks[i].transform.localScale = new Vector3(waveformBlockWidth * .7f, height * _WaveformBlockHeight * .85f, .01f);
         }
+
+        // Setup waveform border
+        _WaveformBorderLine.positionCount = _BorderVerts * 2;
+        
+
+        int borderIndex = 0;
+        for (int i = 0; i < _BorderVerts; i++)
+        {
+            float norm = i / (_BorderVerts - 1f);
+            norm = Mathf.Lerp(-.01f, 1.01f, norm);
+            _WaveformBorderLine.SetPosition(borderIndex, GetPositionOnArc(norm, _WaveformBlockHeight * .5f));
+            borderIndex++;
+        }
+
+        for (int i = 0; i < _BorderVerts; i++)
+        {
+            float norm = 1 - (i / (_BorderVerts - 1f));
+            norm = Mathf.Lerp(-.01f, 1.01f, norm);
+            _WaveformBorderLine.SetPosition(borderIndex, GetPositionOnArc(norm, _WaveformBlockHeight * -.5f));
+            borderIndex++;
+        }
+
+        _WaveformPlayhead.localScale = new Vector3(.03f, _WaveformBlockHeight * .85f, 1);
 
 
         // Create grain blocks
@@ -100,6 +136,11 @@ public class GrainSynthVisualizer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Waveform
+        _WaveformPlayhead.position = GetPositionOnArc(_Emitter._EmissionProps._Playhead, 0, -.03f);
+        _WaveformPlayhead.LookAt(_LookAtPos);
+
+        // Grain timeline
         if (_YAxisPivot != null)
             _YAxisPivot.SetScaleY(_TimelineScale * _TimelineHeightCount);
 
@@ -127,15 +168,22 @@ public class GrainSynthVisualizer : MonoBehaviour
         Gizmos.DrawLine(transform.position, transform.position + -transform.right * _TimelineDistance);
         Gizmos.DrawLine(transform.position, transform.position + transform.up * _TimelineScale * _TimelineHeightCount);
 
+        Vector3 prevPos;
         for (int i = 1; i < 10; i++)
         {
             float norm0 = (i-1) / 9f;
             float norm1 = i / 9f;
 
-            Gizmos.DrawLine(GetPositionOnArc(norm0), GetPositionOnArc(norm1));
+            Gizmos.DrawLine(GetPositionOnArc(norm0, _WaveformBlockHeight *.5f), GetPositionOnArc(norm1, _WaveformBlockHeight * .5f));
         }
 
-        Gizmos.DrawWireSphere(GetPositionOnArc(.1f), .1f);
+        for (int i = 1; i < 10; i++)
+        {
+            float norm0 = (i - 1) / 9f;
+            float norm1 = i / 9f;
+
+            Gizmos.DrawLine(GetPositionOnArc(norm0, _WaveformBlockHeight * -.5f), GetPositionOnArc(norm1, _WaveformBlockHeight * -.5f));
+        }
     }
     #endregion
 
@@ -151,14 +199,15 @@ public class GrainSynthVisualizer : MonoBehaviour
         return pos;
     }
 
-    public Vector3 GetPositionOnArc(float norm)
+    public Vector3 GetPositionOnArc(float norm, float yOffset = 0, float radiusOffset = 0)
     {
         float radians = (-_ArcTotalAngle * .5f * Mathf.Deg2Rad) + (norm * _ArcTotalAngle * Mathf.Deg2Rad);
 
         Vector3 pos = Vector3.zero;
 
-        pos.x = Mathf.Sin(radians) * _ArcRadius;
-        pos.z = Mathf.Cos(radians) * _ArcRadius;
+        pos.x = Mathf.Sin(radians) * (_ArcRadius + radiusOffset);
+        pos.z = Mathf.Cos(radians) * (_ArcRadius + radiusOffset);
+        pos.y = _WaveformHeight + yOffset;
 
         return pos;
     }
@@ -177,23 +226,5 @@ public class GrainSynthVisualizer : MonoBehaviour
         block.gameObject.SetActive(true);
         _BlockCounter++;
         _BlockCounter %= _TimelineBlocks.Length;        
-    }
-
-    private float DecibelToLinear(float dB)
-    {
-        float linear = Mathf.Pow(10.0f, dB / 20.0f);
-        return linear;
-    }
-
-    private float LinearToDecibel(float linear)
-    {
-        float dB;
-
-        if (linear != 0)
-            dB = 20.0f * Mathf.Log10(linear);
-        else
-            dB = -144.0f;
-
-        return dB;
     }
 }
