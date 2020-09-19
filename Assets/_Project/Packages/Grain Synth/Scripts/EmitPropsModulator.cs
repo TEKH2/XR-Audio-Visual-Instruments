9bff4c190e7da08f4e27d4c8498d670859f3e13a
Chris Vik <vik.chris@gmail.com>
2020-09-19 16:02:03 +1000

    Added triggering system to emit props modulation

--- Assets/_Project/Packages/Grain Synth/Scripts/EmitPropsModulator.cs
+++ Assets/_Project/Packages/Grain Synth/Scripts/EmitPropsModulator.cs
@@ -9,26 +9,50 @@ public class EmitPropsModulator : MonoBehaviour
     public ParticleSystem.MinMaxCurve _Playhead;
     public ParticleSystem.MinMaxCurve _Cadence;
     public ParticleSystem.MinMaxCurve _Duration;
+    public ParticleSystem.MinMaxCurve _Transpose;
 
     public float _LoopDuration = 4;
-    float _Timer;
+    private float _Timer;
+    private float _TimerPrevious;
     public float _Norm;
     public bool _Active = true;
+    public bool _Play = true;
+    private bool _PlayPreviously = true;
 
     public bool _Loop = true;
-  
+
+    private bool _ModEnd = false;
+
     // Update is called once per frame
     void Update()
     {
-        if (_Active)
+        if (!_Loop && _Play && !_PlayPreviously)
+        {
+            ResetTimer();
+        }
+
+        if (_Active && _Play)
         {
             _Timer += Time.deltaTime;
             _Timer %= _LoopDuration;
-            _Norm = _Timer / _LoopDuration;
-            _Emitter._EmissionProps._Playhead = GetMinMaxValue(_Playhead, _Norm);
-            _Emitter._EmissionProps._Cadence = GetMinMaxValue(_Cadence, _Norm);
-            _Emitter._EmissionProps._Duration = GetMinMaxValue(_Duration, _Norm);
+
+            if (!_Loop && _Timer < _TimerPrevious)
+            {
+                _Play = false;
+                ResetTimer();
+            }
+            else
+            {
+                _Norm = _Timer / _LoopDuration;
+                _Emitter._EmissionProps._Playhead = GetMinMaxValue(_Playhead, _Norm);
+                _Emitter._EmissionProps._Cadence = GetMinMaxValue(_Cadence, _Norm);
+                _Emitter._EmissionProps._Duration = GetMinMaxValue(_Duration, _Norm);
+                _Emitter._EmissionProps._Transpose = GetMinMaxValue(_Transpose, _Norm);
+            }
         }
+
+        _TimerPrevious = _Timer;
+        _PlayPreviously = _Play;
     }
 
     public float GetMinMaxValue(ParticleSystem.MinMaxCurve minMaxCurve, float norm)
