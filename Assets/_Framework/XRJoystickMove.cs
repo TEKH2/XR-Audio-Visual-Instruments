fc04e6599a9895c4c4dc6360087b41508f210ec2
BradHammond <brad@exp.org.au>
2020-09-19 13:36:38 +1000

    joystick move in

---
+++ Assets/_Framework/XRJoystickMove.cs
@@ -0,0 +1,27 @@
+﻿using EXP.XR;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRJoystickMove : MonoBehaviour
+{
+    XRRig _XRRig;
+
+    public float _Speed = .4f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _XRRig = FindObjectOfType<XRRig>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        Vector2 movement = XRControllers.Instance._LeftControllerFeatures._XRVector2Dict[XRVector2s.PrimaryAxis].Value;
+        Vector3 move3d = new Vector3(movement.x, 0, movement.y);
+        _XRRig.rig.transform.Translate(_XRRig.cameraGameObject.transform.TransformDirection(move3d) * _Speed);
+    }
+}
