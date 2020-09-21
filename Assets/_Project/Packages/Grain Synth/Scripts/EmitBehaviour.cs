738fb5387b33f5e470ea5efb23c1c1c09753bec2
Chris Vik <vik.chris@gmail.com>
2020-09-21 15:45:39 +1000

    Tried to add a state machine and borked a bunch of stuff

--- Assets/_Project/Packages/Grain Synth/Scripts/EmitBehaviour.cs
+++ Assets/_Project/Packages/Grain Synth/Scripts/EmitBehaviour.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Ludiq;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -18,105 +19,127 @@ public class EmitBehaviour : MonoBehaviour
         Idle,
         Moving,
         Collided,
-        Interacting
+        Interacting,
+        Off
     }
-
-    public State _State = State.Idle;
-
-    public EmitterBehavior _IdleBehavior;
-    public EmitterBehavior _MovingBehavior;
-    public EmitterBehavior _CollidingBehavior;
-    public EmitterBehavior _InteractingBehavior;
-
-    public EmitterBehavior _ActiveBehavior;
-
 
     public string _BehaviourName;
     public GrainEmitterAuthoring _Emitter;
 
-    [Header("Global Values")]
-    public float _BehaviourDuration = 4000;
-    private float _Timer;
-    private float _TimerPrevious;
-    [Range(0,1)]
-    public float _Norm;
-   
-    public bool _Play = true;
-    private bool _PlayPreviously = true;
-    public bool _Loop = true;
-    public bool _SilenceWhenDone = true;
+    [Header("Behaviour States")]
+    public State _State = State.Idle;
+    public EmitterBehaviour _ActiveBehaviour;
 
+    public EmitterBehaviour _IdleBehaviour;
+    public EmitterBehaviour _MovingBehaviour;
+    public EmitterBehaviour _CollisionBehaviour;
+    public EmitterBehaviour _InteractionBehaviour;
+
+    [Header("State Configuration")]
+
+    public bool _IdleEnabled = true;
+    public bool _MovingEnabled = true;
+    public bool _CollisionEnabled = true;
+    public bool _InteractionEnabled = true;
+
+    public float _MovingSpeedThreshold = 0.1f;
+    public bool _RotateTriggersMoving = false;
+    public float _RotationSpeedThreshold = 0.1f;
+
+    private Rigidbody _RigidBody;
 
     [Header("DEBUG")]
     public bool _Active = true;
 
     void Start()
     {
-        _IdleBehavior.Init(gameObject);
-        _MovingBehavior.Init(gameObject);
-        _CollidingBehavior.Init(gameObject);
-        _InteractingBehavior.Init(gameObject);
+        if (GetComponent<Rigidbody>() != null)
+            _RigidBody = GetComponent<Rigidbody>();
+        else
+        {
+            _RigidBody = gameObject.AddComponent<Rigidbody>();
+            _RigidBody.isKinematic = true;
+        }
+
+        _IdleBehaviour.Init(gameObject, _Emitter);
+        _MovingBehaviour.Init(gameObject, _Emitter);
+        _CollisionBehaviour.Init(gameObject, _Emitter);
+        _InteractionBehaviour.Init(gameObject, _Emitter);
+
+        if (_IdleEnabled)
+        {
+            _State = State.Idle;
+            _ActiveBehaviour = _IdleBehaviour;
+            _IdleBehaviour._Active = true;
+        }
+        else
+        {
+            _State = State.Off;
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (_CollisionEnabled)
+        {
+            _State = State.Collided;
+            _CollisionBehaviour.SetCollisionData(collision);
+            _CollisionBehaviour.Reset();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        // If not looping, but play has turned to active
-        if (!_Loop && _Play && !_PlayPreviously)
-        {
-            ResetTimer();
-        }
-
-        if (_Active && _Play)
-        {
-            if (!_PlayPreviously)
-                _Emitter._EmissionProps._Playing = true;
-
-            _Timer += Time.deltaTime * 1000;
-            _Timer %= _BehaviourDuration;
-
-            switch (_State)
+        // Clear finished behaviour
+        if (_ActiveBehaviour != null && !_ActiveBehaviour._Active)
+            if (_IdleEnabled)
             {
-                case State.Idle:
-                    _ActiveBehavior = _IdleBehavior;
-                    break;
-                case State.Moving:
-                    _ActiveBehavior = _MovingBehavior;
-                    break;
-                case State.Interacting:
-                    _ActiveBehavior = _InteractingBehavior;
-                    break;
-                case State.Collided:
-                    _ActiveBehavior = _CollidingBehavior;
-                    break;
-            }
-
-
-            // End of non-looped trigger
-            if (!_Loop && _Timer < _TimerPrevious)
-            {
-                _Play = false;
-                ResetTimer();
-
-                if (_SilenceWhenDone)
-                    _Emitter._EmissionProps._Playing = false;
+                _State = State.Idle;
+                _IdleBehaviour._Active = true;
             }
             else
+                _State = State.Off;
+
+        // If behaviour isn't an active collsion - check state in order of priority
+        if (_State != State.Collided)
+        {
+            if (_InteractionEnabled)
             {
-                _Norm = _Timer / _BehaviourDuration;
-                _Emitter._EmissionProps._Playhead = _ActiveBehavior._Playhead.GetValue(_Norm);
-                _Emitter._EmissionProps._Cadence = _ActiveBehavior._Cadence.GetValue(_Norm);
-                _Emitter._EmissionProps._Duration = _ActiveBehavior._Duration.GetValue(_Norm);
-                _Emitter._EmissionProps._Transpose = _ActiveBehavior._Transpose.GetValue(_Norm);
+                // TODO define interaction triggers
             }
+            else if (_MovingEnabled)
+                if (_RigidBody.velocity.magnitude > _MovingSpeedThreshold || (_RotateTriggersMoving && _RigidBody.angularVelocity.magnitude > _RotationSpeedThreshold))
+                {
+                    _State = State.Moving;
+                }
+                else if (_IdleEnabled)
+                    _State = State.Idle;
+                else
+                    _State = State.Off;
         }
 
-        _TimerPrevious = _Timer;
-        _PlayPreviously = _Play;
-    }
+        // Switch to requried behaviour
+        switch (_State)
+        {
+            case State.Idle:
+                _ActiveBehaviour = _IdleBehaviour;
+                break;
+            case State.Moving:
+                _ActiveBehaviour = _MovingBehaviour;
+                break;
+            case State.Interacting:
+                _ActiveBehaviour = _InteractionBehaviour;
+                break;
+            case State.Collided:
+                _ActiveBehaviour = _CollisionBehaviour;
+                break;
+        }
 
-    public void ResetTimer()
-    {
-        _Timer = 0;
+
+        if (_State != State.Off || _ActiveBehaviour != null)
+        {
+            _ActiveBehaviour.OnUpdate();
+        }
     }
 }
---
+++ Assets/_Project/Packages/Grain Synth/Scripts/EmitterBehaviour.cs
@@ -0,0 +1,233 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EmitBehavior", menuName = "Grain Synth/Emitter Behavior", order = 1)]
+public class EmitterBehaviour : ScriptableObject
+{
+    public enum EventType
+    {
+        OneShot,
+        Hold,
+        Loop
+    }
+
+    [Header("Event")]
+    public EventType _EventType;
+    public int _BehaviourDuration = 1000;
+    private float _Timer = 0;
+    private float _TimerNorm = 0;
+    public bool _Active = false;
+    private bool _Started = false;
+    private bool _Ended = false;
+
+    [Header("Grain Parameters")]
+    public BehaviourParameter _Playhead;
+    public BehaviourParameter _Cadence;
+    public BehaviourParameter _Duration;
+    public BehaviourParameter _Transpose;
+
+    private readonly Vector2 PlayheadRange = new Vector2(0, 1);
+    private readonly Vector2 CadenceRange = new Vector2(3, 200);
+    private readonly Vector2 DurationRange = new Vector2(5, 500);
+    private readonly Vector2 TransposeRange = new Vector2(-3, 3);
+
+    private GrainEmitterAuthoring _Emitter;
+    private Collision _Collision;
+
+    public void Init(GameObject go, GrainEmitterAuthoring emitter)
+    {
+        _Playhead.SetGameObject(go, PlayheadRange);
+        _Cadence.SetGameObject(go, CadenceRange);
+        _Duration.SetGameObject(go, DurationRange);
+        _Transpose.SetGameObject(go, TransposeRange);
+
+        _Emitter = emitter;
+    }
+
+    public void Reset()
+    {
+        _Active = true;
+        _Started = false;
+        _Ended = false;
+    }
+
+
+    public void OnUpdate()
+    {
+        if (_Active)
+        {
+            // When starting new playback, reset the timer unless its a loop
+            if (!_Started)
+            {
+                _Started = true;
+                _Ended = false;
+                if (_EventType != EventType.Loop)
+                    _Timer = 0;
+            }
+
+            // Update timer, or deactivate if OneShot and has ended
+            if (!_Ended)
+            {
+                _Timer += Time.deltaTime * 1000;
+
+                if (_EventType == EventType.OneShot)
+                    if (_Timer > _BehaviourDuration)
+                    {
+                        _Ended = true;
+                        _Active = false;
+                        _Collision = null;
+                    }
+                else if (_EventType == EventType.Hold)
+                    if (_Timer > _BehaviourDuration)
+                    {
+                        _Ended = true;
+                        _Timer = _BehaviourDuration - 1;
+                    }
+                else if (_EventType == EventType.Loop)
+                {
+                    _Timer += Time.deltaTime * 1000;
+                    _Timer %= _BehaviourDuration;
+                }
+            }
+
+            if (_Active)
+            {
+                _TimerNorm = _Timer / _BehaviourDuration;
+                _Emitter._EmissionProps._Playhead = _Playhead.GetValue(_TimerNorm, _Collision);
+                _Emitter._EmissionProps._Cadence = _Cadence.GetValue(_TimerNorm, _Collision);
+                _Emitter._EmissionProps._Duration = _Duration.GetValue(_TimerNorm, _Collision);
+                _Emitter._EmissionProps._Transpose = _Transpose.GetValue(_TimerNorm, _Collision);
+            }
+        }
+    }
+
+    public void SetCollisionData(Collision collision)
+    {
+        _Collision = collision;
+    }
+}
+
+[System.Serializable]
+public class BehaviourParameter
+{ 
+    public void SetGameObject(GameObject gameObject, Vector2 range)
+    {
+        _AttachedGameObject = gameObject;
+        _RigidBody = gameObject.GetComponent<Rigidbody>();
+        _OutputLow = range.x;
+        _OutputHigh = range.y;
+    }
+
+    public enum ModulationType
+    {
+        Time,
+        Position,
+        Velocity,
+        Rotation,
+        Scale,
+        Collision,
+        Off
+    }
+
+    public enum AxisSelection
+    {
+        Magnitude,
+        X,
+        Y,
+        Z
+    }
+
+    private GameObject _AttachedGameObject;
+    private Rigidbody _RigidBody;
+
+    public ParticleSystem.MinMaxCurve _Curve;
+
+    [Header("Input")]
+    public ModulationType _ModulationType;
+    public AxisSelection _Axis;
+
+    public float _InputLow = 0;
+    public float _InputHigh = 1;
+
+    [Header("Output")]
+    public float _OutputLow = 0;
+    public float _OutputHigh = 1;
+
+    [Header("DEBUG")]
+    [Range(0, 1)]
+    public float _TestInput = 0;
+    public float _ModulationInput = 0;
+
+    public float GetValue(float time, Collision collision)
+    {
+        if (_ModulationType == ModulationType.Time)
+            _ModulationInput = time;
+        else
+        {
+            switch (_ModulationType)
+            {
+                case ModulationType.Position:
+                    _ModulationInput = GetValue(_AttachedGameObject.transform.position, _Axis);
+                    break;
+                case ModulationType.Velocity:
+                    _ModulationInput = GetValue(_RigidBody.velocity, _Axis);
+                    break;
+                case ModulationType.Rotation:
+                    _ModulationInput = GetValue(_AttachedGameObject.transform.rotation.eulerAngles, _Axis);
+                    break;
+                case ModulationType.Scale:
+                    _ModulationInput = GetValue(_AttachedGameObject.transform.localScale, _Axis);
+                    break;
+                case ModulationType.Collision:
+                    if (collision != null)
+                        _ModulationInput = GetValue(collision.impulse, _Axis);
+                    break;
+                case ModulationType.Off:
+                    _ModulationInput = _TestInput;
+                    break;
+                default:
+                    break;
+            }
+
+            _ModulationInput = GrainSynthSystem.Map(_ModulationInput, _InputLow, _InputHigh, _OutputLow, _OutputHigh);
+        }
+
+        return GetMinMaxValue(_Curve, _ModulationInput);
+    }
+
+    public float GetValue(Vector3 input, AxisSelection axis)
+    {
+        switch (axis)
+        {
+            case AxisSelection.X:
+                return input.x;
+            case AxisSelection.Y:
+                return input.y;
+            case AxisSelection.Z:
+                return input.z;
+            case AxisSelection.Magnitude:
+                return Vector3.Magnitude(input);
+            default:
+                return 0;
+        }
+    }
+
+    public float GetMinMaxValue(ParticleSystem.MinMaxCurve minMaxCurve, float norm)
+    {
+        switch (minMaxCurve.mode)
+        {
+            case ParticleSystemCurveMode.Constant:
+                return minMaxCurve.constant;
+            case ParticleSystemCurveMode.Curve:
+                return minMaxCurve.curve.Evaluate(norm);
+            case ParticleSystemCurveMode.TwoConstants:
+                return Mathf.Lerp(minMaxCurve.constantMin, minMaxCurve.constantMax, UnityEngine.Random.value);
+            case ParticleSystemCurveMode.TwoCurves:
+                return Mathf.Lerp(minMaxCurve.curveMin.Evaluate(norm), minMaxCurve.curveMax.Evaluate(norm), UnityEngine.Random.value);
+            default:
+                return 0;
+        }
+    }
+}
