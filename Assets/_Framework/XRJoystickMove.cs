8c3988f03439ffd7c61e83dacd546a3a49e7be35
BradHammond <brad@exp.org.au>
2020-12-15 08:57:36 +1100

    fixed xr joystick move error where XR controller script wasnt turned on

--- Assets/_Framework/XRJoystickMove.cs
+++ Assets/_Framework/XRJoystickMove.cs
@@ -19,9 +19,11 @@ public class XRJoystickMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-      
-        Vector2 movement = XRControllers.Instance._LeftControllerFeatures._XRVector2Dict[XRVector2s.PrimaryAxis].Value;
-        Vector3 move3d = new Vector3(movement.x, 0, movement.y);
-        _XRRig.rig.transform.Translate(_XRRig.cameraGameObject.transform.TransformDirection(move3d) * _Speed);
+        if (XRControllers.Instance._LeftControllerFeatures != null)
+        {
+            Vector2 movement = XRControllers.Instance._LeftControllerFeatures._XRVector2Dict[XRVector2s.PrimaryAxis].Value;
+            Vector3 move3d = new Vector3(movement.x, 0, movement.y);
+            _XRRig.rig.transform.Translate(_XRRig.cameraGameObject.transform.TransformDirection(move3d) * _Speed);
+        }
     }
 }
