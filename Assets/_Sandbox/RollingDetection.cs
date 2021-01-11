2fd3a6cc3a0a0ef86389f17ff0909bd42a89af3c
BradHammond <brad@exp.org.au>
2021-01-11 17:46:05 +1100

    rolling detection

---
+++ Assets/_Sandbox/RollingDetection.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingDetection : MonoBehaviour
+{
+    public enum State
+    {
+        NotColliding,
+        Colliding,
+    }
+
+    public State _State = State.NotColliding;
+    public PhysicMaterial _CollidedPhysicsMat;
+    public float _RollSpeed;
+
+    Rigidbody _RB;
+
+
+    private void Start()
+    {
+        _RB = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        _State = State.Colliding;
+        _CollidedPhysicsMat = collision.collider.material;
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        _State = State.Colliding;
+        _RollSpeed = _RB.angularVelocity.magnitude;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        _State = State.NotColliding;
+        _CollidedPhysicsMat = null;
+        _RollSpeed = 0;
+    }
+}
